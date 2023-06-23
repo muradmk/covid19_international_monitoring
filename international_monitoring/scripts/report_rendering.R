999d98f74237e7be3940986687e2af34583a96c4
muradmk <40899188+muradmk@users.noreply.github.com>
2023-06-23 11:14:27 -0400

    Delete international_monitoring directory

--- international_monitoring/scripts/report_rendering.R
+++
@@ -1,4 +0,0 @@
-rmarkdown::render('international_monitoring.Rmd',
-                  output_file = paste0('international_monitoring_', format(Sys.Date(), "%d-%b-%Y"), 
-                                      '.html', sep=''),
-                  output_dir = "outputs/")
